b7dcf2707506d8c328ac72bd6fafbd64de05c76e
yuchia0221 <46244904+yuchia0221@users.noreply.github.com>
2019-01-13 16:56:28 +0800

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -1,11 +1,10 @@
 import csv
 from collections import namedtuple
 
-country_info = namedtuple("country_info",
-                          "name asset gold population solider weapon food_speed wood_speed mineral_speed oil_speed food wood mineral oil")
 
 with open("國家資訊.csv", "r") as csv_file:
+    country_info = namedtuple("country_info",
+                              "name asset gold population solider weapon food_speed wood_speed mineral_speed oil_speed food wood mineral oil")
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
     country_list = [country_info(*i) for i in csv_reader]
-
