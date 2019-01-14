32f1906b5fbf32af14397bbfa4133cf3d6a83b1a
yuchia0221 <46244904+yuchia0221@users.noreply.github.com>
2019-01-14 13:05:44 +0800

    Update Country.py

--- Country.py
+++ Country.py
@@ -4,14 +4,14 @@ from read_file import country_list, nameDict
 class Country:
     """This class contains basic country attributes"""
 
-    def __init__(self, ID, name, asset, gold, population, solIDer, weapon,
+    def __init__(self, ID, name, asset, gold, population, solider, weapon,
                  food_speed, wood_speed, mineral_speed, oil_speed, food, wood, mineral, oil):
-        self._ID = int(ID)
+        self.ID = int(ID)
         self._name = nameDict[name]
         self._asset = float(asset)
         self._gold = int(gold)
         self._population = int(population)
-        self._solIDer = int(solIDer)
+        self._solider = int(solider)
         self._food_speed = float(food_speed)
         self._wood_speed = float(wood_speed)
         self._mineral_speed = float(mineral_speed)
@@ -22,24 +22,134 @@ class Country:
         self._oil = int(oil)
 
     def __str__(self):
-        return f"This country is {self.name}, food:{self.food}, wood:{self.wood}, mineral:{self.mineral}, oil:{self.oil}"
+        return f"This country is {self._name}, food:{self.food}, wood:{self.wood}, mineral:{self.mineral}, oil:{self.oil}"
 
     def __lt__(self, other):
         return self.asset < other.asset
 
     @property
-    def ID(self):
-        return self._ID
+    def asset(self):
+        return self._asset
 
-    @ID.setter
-    def ID(self, value):
-        if not 1 <= value <= 10:
-            raise ValueError("ID must between 1 to 10")
-        self._ID = value
+    @asset.setter
+    def asset(self, value):
+        if value < 0:
+            raise ValueError("asset must be positive")
+        self._asset = value
+
+    @property
+    def gold(self):
+        return self._gold
+
+    @gold.setter
+    def gold(self, value):
+        if value < 0:
+            raise ValueError("gold must be positive")
+        self._gold = value
+
+    @property
+    def population(self):
+        return self._population
+
+    @gold.setter
+    def population(self, value):
+        if value < 0:
+            raise ValueError("population must be positive")
+        self._population = value
+
+    @property
+    def solider(self):
+        return self._solider
+
+    @gold.setter
+    def solider(self, value):
+        if value < 0:
+            raise ValueError("solider must be positive")
+        self._solider = value
+
+    @property
+    def food_speed(self):
+        return self._food_speed
+
+    @food_speed.setter
+    def food_speed(self, value):
+        if value < 0:
+            raise ValueError("food_speed must be positive")
+        self._food_speed = value
+
+    @property
+    def wood_speed(self):
+        return self._wood_speed
+
+    @wood_speed.setter
+    def food_speed(self, value):
+        if value < 0:
+            raise ValueError("wood_speed must be positive")
+        self._wood_speed = value
+
+    @property
+    def mineral_speed(self):
+        return self._mineral_speed
+
+    @wood_speed.setter
+    def mineral_speed(self, value):
+        if value < 0:
+            raise ValueError("mineral_speed must be positive")
+        self._mineral_speed = value
+
+    @property
+    def oil_speed(self):
+        return self._oil_speed
+
+    @oil_speed.setter
+    def oil_speed(self, value):
+        if value < 0:
+            raise ValueError("oil_speed must be positive")
+        self._oil_speed = value
+
+    @property
+    def food(self):
+        return self._food
+
+    @food.setter
+    def food(self, value):
+        if value < 0:
+            raise ValueError("food must be positive")
+        self._food = value
+
+    @property
+    def wood(self):
+        return self._wood
+
+    @wood.setter
+    def wood(self, value):
+        if value < 0:
+            raise ValueError("wood must be positive")
+        self._wood = value
+
+    @property
+    def mineral(self):
+        return self._mineral
+
+    @mineral.setter
+    def mineral(self, value):
+        if value < 0:
+            raise ValueError("mineral must be positive")
+        self._mineral = value
+
+    @property
+    def oil(self):
+        return self._oil
+
+    @oil.setter
+    def oil(self, value):
+        if value < 0:
+            raise ValueError("oil must be positive")
+        self._oil = value
 
     @classmethod
     def from_tuple(cls, data):
-        return cls(data.id, data.name, float(data.asset), int(data.gold), int(data.population), int(data.solIDer), float(data.weapon),
+        return cls(data.id, data.name, float(data.asset), int(data.gold), int(data.population), int(data.solider), float(data.weapon),
                    float(data.food_speed), float(data.wood_speed), float(data.mineral_speed), float(data.oil_speed),
                    int(data.food), int(data.wood), int(data.mineral), int(data.oil))
 
