4ad85e820683dc66244abcad559e5170327fe261
yuchia0221 <46244904+yuchia0221@users.noreply.github.com>
2019-03-16 22:17:33 +0800

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -5,8 +5,8 @@ from collections import namedtuple
 country_info = namedtuple("country_info",
                           "id name asset gold population solider weapon food_speed wood_speed mineral_speed oil_speed food wood mineral oil")
 
-nameDict = {"亞特蘭提斯": "Atlantis", "阿斯嘉": "Asgard", "奧林匹斯": "Olympus", "瓦干達": "Wakanda", "香格里拉": "ShangriLa",
-            "瓦拉納西": "Varanasi", "瑪雅": "Maya", "黃金之都": "ElDorado", "游牧": "Mongolia", "澳洲": "Australia"}
+nameDict = {"亞特蘭提斯": "Atlantis", "阿斯嘉": "Asgard", "奧林帕斯": "Olympus", "瓦干達": "Wakanda", "香格里拉": "ShangriLa",
+            "瓦拉納西": "Varanasi", "瑪雅": "Maya", "塔爾塔洛斯": "Tartarus", "特奧蒂瓦坎": "Teotihuacan", "復活節島": "EasterIsland"}
 
 with open("國家資訊.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file)
