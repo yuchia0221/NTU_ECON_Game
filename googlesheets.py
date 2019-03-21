31b42ff16b134aaad223ece22bbd3c218898e7b4
yuchia0221 <46244904+yuchia0221@users.noreply.github.com>
2019-03-21 10:17:24 +0800

    Update googlesheets.py

--- googlesheets.py
+++ googlesheets.py
@@ -1,6 +1,4 @@
 import gspread
-# import time
-# import random
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
@@ -10,7 +8,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("google.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("國家資訊表").sheet1
 data = sheet.get_all_records()
-print(data[0].values())
+print(data)
 
 
 # print(data)
