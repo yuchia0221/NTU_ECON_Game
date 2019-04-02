e522f41eeadf8b5cf28fde71dceaa37c9a646040
yuchia0221 <46244904+yuchia0221@users.noreply.github.com>
2019-04-02 15:20:53 +0800

    Update function.py

--- function.py
+++ function.py
@@ -6,6 +6,8 @@ from Country import (Atlantis, Asgard, Olympus, Wakanda, ShangriLa,
 
 
 def read_file():
+    """讀取位於Google雲端的試算表，回傳list"""
+
     country_info = namedtuple("country_info",
                               "id name wonders gold population weapon air food_speed wood_speed steel_speed stone_speed food wood steel stone")
 
@@ -16,17 +18,15 @@ def read_file():
     client = gspread.authorize(creds)
     sheet = client.open("國家資訊表").sheet1
     data = sheet.get_all_records()
-    raw_data = [country_info(*i.values()) for i in data]
 
-    return raw_data
+    return [country_info(*i.values()) for i in data]
 
 
 def createCountry():
-    name = {"亞特蘭提斯": Atlantis, "阿斯嘉": Asgard, "奧林帕斯": Olympus, "瓦干達": Wakanda,
-            "香格里拉": ShangriLa, "瓦拉納西": Varanasi, "瑪雅": Maya, "塔爾塔洛斯": Tartarus,
-            "特奧蒂瓦坎": Teotihuacan, "復活節島": EasterIsland}
+    """透過read_file函數建立Class"""
 
-    data = read_file()
-    return_list = [name[i.name](*i) for i in data]
+    class_list = {"亞特蘭提斯": Atlantis, "阿斯嘉": Asgard, "奧林帕斯": Olympus, "瓦干達": Wakanda,
+                  "香格里拉": ShangriLa, "瓦拉納西": Varanasi, "瑪雅": Maya, "塔爾塔洛斯": Tartarus,
+                  "特奧蒂瓦坎": Teotihuacan, "復活節島": EasterIsland}
 
-    return return_list
+    return [class_list[i.name](*i) for i in read_file()]
