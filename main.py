7b90e621581ea3ba6666946b761c4f67b6ac32c4
yuchia0221 <46244904+yuchia0221@users.noreply.github.com>
2019-04-27 00:09:00 +0800

    add time calculate

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from function import (read_file, write_country_file, createCountry,
                       handle_action, production, read_card, card, war, wonder, write_wonders)
+from time import time
 
 if __name__ == "__main__":
+
+    before = time()
+
     countryDict = createCountry()
     actionList = handle_action()
     cardDict = read_card()
     wonderlist = read_file("世界奇觀")
 
-    print("讀取檔案完成")
+    after = time()
+    print(f"讀取檔案完成，共花費{after - before:.1f}s")
+
+    before = time()
 
     for i in actionList:
         production(countryDict, i.name, i.produceList, i.occupyMan)
@@ -28,15 +35,20 @@ if __name__ == "__main__":
 
     wonder(countryDict, wonderlist, actionList)
 
-    print("基本函數完成")
-    print("開始寫檔完成")
+    after = time()
+    print(f"基本函數完成，共花費{after - before:.1f}s")
+
+    print("開始寫檔")
+    before = time()
 
     for i in countryDict.values():
         i.population += 100
 
     # roundnow = "二"
-    write_country_file(countryDict)
-    countryName = ['亞特蘭提斯', '阿斯嘉', '奧林帕斯', '瓦干達', '香格里拉',
-                   '瓦拉納西', '瑪雅', '塔爾塔洛斯', '特奧蒂瓦坎', '復活節島']
-    write_wonders(countryDict)
-    print("寫檔完成")
+    # write_country_file(countryDict)
+    # countryName = ['亞特蘭提斯', '阿斯嘉', '奧林帕斯', '瓦干達', '香格里拉',
+    #                '瓦拉納西', '瑪雅', '塔爾塔洛斯', '特奧蒂瓦坎', '復活節島']
+    # write_wonders(countryDict)
+
+    after = time()
+    # print(f"寫檔完成，共花費{after - before:.1f}s")
