83e2a692a95ec5e7791a760fbe39bcfedc979eb0
curtislo226 <curtis0226@gmail.com>
2019-04-27 16:48:21 +0800

    Revert "Merge branch 'master' of https://github.com/yuchia0221/NTU_ECON_Game"
    This reverts commit f4070cfd4e2f61352e7c77dd7008772afc188962, reversing
    changes made to 3683a252ded08bec320c9c184e06ea4753daa4ac.

--- trade.py
+++ trade.py
@@ -18,13 +18,12 @@ if __name__ == "__main__":
     print(trade_info)
     for i in countryDict.values():
         print(i.to_list())
-
     # 定價
-    price_list = []                                                     # [糧食, 木頭, 鐵礦, 石頭]
-    demand = [0, 0, 0, 0]                                               # 物資的總需求，總供給 [糧食, 木頭, 鐵礦, 石頭]
+    price_list = []                 # [糧食, 木頭, 鐵礦, 石頭]
+    demand = [0, 0, 0, 0]           # 物資的總需求，總供給 [糧食, 木頭, 鐵礦, 石頭]
     supply = [0, 0, 0, 0]
-    for j in trade_info:                                    # [時間, 國家名, 賣糧, 賣木, 賣鐵, 賣石, 買糧, 買木, 買鐵, 買石]
-        if countryDict[j[1]].food < j[2]:                   # 如果物資不夠賣，將當回賣的物資量等同現有物資存量，並顯示錯誤
+    for j in trade_info:        # [時間, 國家名, 賣糧, 賣木, 賣鐵, 賣石, 買糧, 買木, 買鐵, 買石]
+        if countryDict[j[1]].food < j[2]:                           # 如果物資不夠賣，將當回賣的物資量等同現有物資存量，並顯示錯誤
             j[2] = countryDict[j[1]].food
             print(f"{j[1]} doesn't have enough resources of food")
         elif countryDict[j[1]].wood < j[3]:
@@ -37,17 +36,18 @@ if __name__ == "__main__":
             j[5] = countryDict[j[1]].stone
             print(f"{j[1]} doesn't have enough resources of stone")
 
-        for i in range(4):                                              # 加總供給與需求
+        for i in range(4):                                          # 加總供給與需求
             supply[i] += j[i + 2]
             demand[i] += j[i + 6]
 
     for i, j in zip(demand, supply):
-        price_list.append(trade_function(i, j))                         # 算出均衡價格
+        price_list.append(trade_function(i, j))  # 算出均衡價格
 
     print(price_list)
 
     # 買賣
-    for i in trade_info:                                    # [時間, 國家名, 賣糧, 賣木, 賣鐵, 賣石, 買糧, 買木, 買鐵, 買石]
+    for i in trade_info:            # [時間, 國家名, 賣糧, 賣木, 賣鐵, 賣石, 買糧, 買木, 買鐵, 買石]
+
         # 執行賣物資的動作
         countryDict[i[1]].food -= i[2]
         countryDict[i[1]].wood -= i[3]
@@ -58,11 +58,11 @@ if __name__ == "__main__":
 
         # 執行買物資的動作
         for j in range(4):
-            if countryDict[i[1]].gold < int(price_list[j] * i[j + 6]):  # 如果黃金不夠買當項物資
-                print(f"{i[1]} doesn't have enough resources of gold")  # 顯示錯誤訊息
+            if countryDict[i[1]].gold < int(price_list[j] * i[j + 6]):          # 如果黃金不夠買當項物資
+                print(f"{i[1]} doesn't have enough resources of gold")          # 顯示錯誤訊息
                 i[j + 6] = 0
-                while(countryDict[i[1]].gold > int(price_list[j] * (i[j + 6] + 500))):
-                    i[j + 6] += 500                                     # 以500為間隔，從0開始，直到他買得起結束
+                while(countryDict[i[1]].gold > int(price_list[j] * (i[j + 6] + 500))):  # 以500為間隔，從0開始，直到他買得起結束
+                    i[j + 6] += 500
 
             countryDict[i[1]].gold -= int(price_list[j] * i[j + 6])
 
